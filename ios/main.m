c2522b6ce96f744ac37c907eb9e83ea056dab7c6
Eugene Yaroslavtsev <exhaze@gmail.com>
2012-01-21 04:34:49 -0800

    added cocos 2d-x base

---
+++ ios/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Untitled
+//
+//  Created by exhaze on 1/21/12.
+//  Copyright Unified Color Technologies 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+	NSAutoreleasePool *pool = [NSAutoreleasePool new];
+	int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+	[pool release];
+	return retVal;
+}
